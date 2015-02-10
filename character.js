33e4f58ea2e1e668938af59fd2b90d5a53a10eb7
zachinator95 <zachary.brunner@student.centraltech.edu>
2015-02-10 10:44:42 -0600

    z

---
+++ character.js
@@ -0,0 +1,127 @@
+var characterName = {
+	name: "Character Name",
+	attributes: ["attribute1", "attribute2", "attribute"]
+}
+var hulk ={
+	name:"Hulk",
+	attributes:["Strength","Short Temper","Destructive","anti-hero"]
+}
+ var scoobyDoo = {
+ 	name:"Scooby-Doo",
+ 	attributes:["Coward","Mystery Solver","Loyal","Dog"]
+ }
+var darthVader = {
+	name:"Darth Vader",
+	attributes:["Reckless","Cyborg","Villian"]
+}
+var mickeyMouse ={
+	name:"Mickey Mouse",
+	attributes:["Lovable","Friendly","Kind","Mascot"]
+}
+var pacMan ={
+	name:"Pac Man",
+	attributes:["Eating","Yellow"]
+}
+var elsa ={
+	name:"Elsa",
+	attributes:["Ice Power","Nervous"]
+}
+var donkeyKong ={
+	name:"Donkey Kong",
+	attributes:["Gorilla","Bannas","Pride"]
+}
+var bugsBunny = {
+	name:"Bugs Bunny",
+	attributes:["Trickster","Smart","Carrots"]
+}
+var olaf ={
+	name:"Olaf",
+	attributes:["Snowman","Good-Hearted","Summer"]
+}
+var daffyDuck={
+	name:"Daffy Duck",
+	attributes:["Hot Head","ScrewBall","Greedy"]
+}
+var storm = {
+	name:"Storm",
+	attributes:["Weather Manipulation","Strategists"]
+}
+var garfield = {
+	name:"Garfield",
+	attributes:["Lazy"]
+}
+var porkyPig = {
+	name:"Porky Pig",
+	attributes:["Stutters"]
+}
+
+var taz={
+	name:"Taz",
+	attributes:["Insatiable Appetite","Multitasker","Devil"]
+}
+
+var katnissEverdeen = {
+	name:"Katniss Everdeen",
+	attributes:["Hunter","Survivor"]
+}
+
+var ironMan = {
+	name:"Iron Man",
+	attributes:["armor"]
+}
+var belle = {
+	name:"Belle",
+	attributes:["books"]
+}
+var yosemiteSam = {
+	name:"Yosemite Sam",
+	attributes:["Explosive"]
+}
+var chewbacca = {
+	name:"Chewbacca",
+	attributes:["co-pilot",]
+}
+var captainAmerica = {
+	name:"Captain America",
+	attributes:["Patriotic"]
+}
+var maleficent={
+	name:"Maleficent",
+	attributes:["Fairy"]
+}
+var anna={
+	name:"Anna",
+	attributes:["Confidence"]
+}
+var wolverine={
+	name:"Wolverine",
+	attributes:["Berserker"]
+}
+var thor={
+	name:"Thor",
+	attributes:["Thunder"]
+}
+var donaldDuck={
+	name:"Donald Duck",
+	attributes:["sailor"]
+}
+var princessLeia = {
+	name:"Princess Leia",
+	attributes:["Politician"]
+}
+var blackWidow={
+	name:"Black Widow",
+	attributes:["Spy"]
+}
+var hanSolo={
+	name:"Han Solo",
+	attributes:["pirate"]
+}
+var laraCroft={
+	name:"Lara Croft",
+	attributes:["archaeologists"]
+}
+var yoshi ={
+	name:"Yoshi",
+	attributes:["dinosaur"]
+}
